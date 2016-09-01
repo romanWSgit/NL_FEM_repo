2d3ffdd0c24adec940b7cb852939e3d3b5e770ff
Roman Wallner- Silberhuber <roman_w_s@me.com>
2016-09-01 16:03:17 +0200

    "Changed bound.m to penalty-method...trial"

--- bound.m
+++ bound.m
@@ -1,14 +1,14 @@
 function[K_mat,F_vec]=bound(K_mat,F_vec,bc_nodes)
 	
-	
+	alpha = 100000;
 	for i=1:length(bc_nodes)
 		pos=bc_nodes(i);
-		for j=1:size(K_mat)
-			K_mat(pos,j)=0;     % Matrizeneinträge Null setzen
-            K_mat(j,pos)=0;     % Matrizeneinträge Null setzen
-		end
-		K_mat(pos,pos)=1;       % "pos" Eins setzen
-		F_vec(pos)=0; % Kraftvektor auf den jeweiligen Wert setzten
+		%for j=1:size(K_mat)
+			%K_mat(pos,j)=K_mat(pos,j)*alpha;     % Matrizeneinträge Null setzen
+            %K_mat(j,pos)=K_mat(pos,j)*alpha;;     % Matrizeneinträge Null setzen
+		%end
+		K_mat(pos,pos)=K_mat(pos,pos)*alpha;       % "pos" Eins setzen
+		F_vec(pos)=F_vec(pos)*alpha; % Kraftvektor auf den jeweiligen Wert setzten
 	end	
 end
 

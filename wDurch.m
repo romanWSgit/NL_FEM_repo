215c13bd803541eb21450a51e38cf8998ff44e9d
Roman Wallner- Silberhuber <roman_w_s@me.com>
2016-09-09 15:23:25 +0200

    "Added Single Load algebric solution"

--- wDurch.m
+++ wDurch.m
@@ -1,3 +1,6 @@
 function w=wDurch(x,l,q0,EIy)
-w=1/(EIy)*(1/24 *q0*x^4 - 1/6 *q0*l*x^3 + 1/4 * q0*l^2 * x^2);
+    %w=1/(EIy)*(1/24 *q0*x^4 - 1/6 *q0*l*x^3 + 1/4 * q0*l^2 * x^2); % Streckenlast
+
+    w = (q0*l^3)/(3*EIy);
+
 end
